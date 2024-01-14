4c1ba11cae68875d6891adeebd6483c93eb5a02e
2439905184 <2439905184@qq.com>
2024-01-14 16:09:16 +0800

    🐞 fix: 修复jsonToOL的bug

--- js/jsonToOl.js
+++ js/jsonToOl.js
@@ -3,14 +3,12 @@ function jsonToOl(p_json,p_root)
 {
     for (var title in p_json) 
     {  
-    var link = p_json[title];  
-    var listItem = document.createElement('li');  
-    var listText = document.createTextNode(title);  
-    var linkElement = document.createElement('a');  
-    linkElement.href = link;  
-    linkElement.appendChild(listText);  
-    listItem.appendChild(linkElement);  
-    var ol = p_root
-    ol.appendChild(listItem);  
+        var link_text = p_json[title]
+        var listItem = document.createElement("li")
+        var linkItem = document.createElement("a")
+        linkItem.innerText = title
+        linkItem.href = link_text
+        listItem.appendChild(linkItem)
+        p_root.appendChild(listItem) 
     }
 }
